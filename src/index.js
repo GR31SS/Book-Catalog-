fcc2965ef8cbad4f8fc8c0c479078e9f5001f3f6
Macaulay-Reiss Christian <macaulay-reisschristian@pc-203-83.customer.card.castle.lan>
2018-11-15 11:26:42 +0000

    Created the Create, Edit, and show pages. and made small changes to the index.js that effected the build"

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Edit from './components/Edit';
 import Create from './components/Create';
 import Show from './components/Show';
@@ -21,4 +19,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-registerServiceWorker();
