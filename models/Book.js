e64f6c5102666c33008f344a33342b1e0c9f090c
Macaulay-Reiss Christian <macaulay-reisschristian@pc-203-83.customer.card.castle.lan>
2018-11-15 11:05:35 +0000

    Added new Components for Create, Edit, Show. Updated the Book.js Code and Index.js code to use the new components

--- models/Book.js
+++ models/Book.js
@@ -8,6 +8,6 @@ var BookSchema = new mongoose.Schema({
     published_date: { type: Date },
     publisher: String,
     updated_date: { type: Date, default: Date.now },
-});
+  });
 
 module.exports = mongoose.model('Book', BookSchema);
